d1d5179f087e3e43a0569f2647b672c2d6af9a74
biaheart <44721134+biaheart@users.noreply.github.com>
2020-06-01 19:56:20 +0800

    修改了潮流计算程序
    因为生成节点导纳矩阵的逻辑改变了，相应的潮流计算程序也相应修改

--- flaskweb/lib_packages/Admittance_Matrix_Class.py
+++ flaskweb/lib_packages/Admittance_Matrix_Class.py
@@ -26,10 +26,15 @@ class Admittancematrix:  # 建立节点导纳矩阵类
         self.admittance_matrix = self.admittance_matrix + self.part_matrix
         self.part_matrix = np.zeros((self.bus_num, self.bus_num), dtype=complex)
 
-    def add(self, node1, node2, resistance, reactance, susp):  # 增加支路的支路追加法
+    def add(self, node1, node2, resistance, reactance, total_b):  # 增加支路的支路追加法
+        if resistance == 0 and reactance == 0:
+            return
+        impedance = complex(resistance, reactance)  # 计算阻抗
+        admittance = 1 / impedance  # 计算导纳
+        average_b = complex(0, total_b / 2)  # 计算分配在两端母线上的电纳
         node1 = int(node1)
         node2 = int(node2)
-        if node1 > self.bus_num or node2 > self.bus_num:    # 判断是否需要扩大矩阵，若输入节点编号大于当前矩阵的阶数，则扩大矩阵
+        if node1 > self.bus_num or node2 > self.bus_num:   # 对矩阵进行扩大
             if node1 > node2:
                 max_node = node1
             else:
@@ -47,47 +52,54 @@ class Admittancematrix:  # 建立节点导纳矩阵类
                 self.part_matrix = np.column_stack((self.part_matrix, c2))
                 k = k + 1
             self.bus_num = max_node
-        self.part_matrix[node1 - 1][node1 - 1] = susp / 2  # 支路电纳的叠加
-        self.part_matrix[node2 - 1][node2 - 1] = susp / 2
-        self.admittance_matrix = self.admittance_matrix + self.part_matrix
-        self.part_matrix = np.zeros((self.bus_num, self.bus_num), dtype=complex)  # 辅助矩阵置零
-        if resistance == 0 and reactance == 0:
-            return
-        impedance = complex(resistance, reactance)  # 计算阻抗
-        admittance = 1 / impedance  # 计算导纳
-        if node1 != node2:  # 判断是串联支路
+        if total_b != 0:    # 对节点导纳矩阵添加并联支路电纳
+            self.part_matrix[node1 - 1][node1 - 1] = average_b
+            self.part_matrix[node2 - 1][node2 - 1] = average_b
+            self.admittance_matrix = self.admittance_matrix + self.part_matrix
+            self.part_matrix = np.zeros((self.bus_num, self.bus_num), dtype=complex)  # 辅助矩阵置零
+        if node1 != node2:  # 判断不是并联导纳
             self.part_matrix[node1 - 1][node1 - 1] = admittance  # 根据增加支路修改辅助矩阵元素
             self.part_matrix[node2 - 1][node2 - 1] = admittance
             self.part_matrix[node1 - 1][node2 - 1] = -admittance
             self.part_matrix[node2 - 1][node1 - 1] = -admittance
             self.admittance_matrix = self.admittance_matrix + self.part_matrix  # 支路追加
             self.part_matrix = np.zeros((self.bus_num, self.bus_num), dtype=complex)  # 辅助矩阵置零
+        else:  # 并联导纳
+            self.part_matrix[node1 - 1][node1 - 1] = admittance
+            self.admittance_matrix = self.admittance_matrix + self.part_matrix
+            self.part_matrix[node1 - 1][node1 - 1] = 0
 
-    def minus(self, node1, node2, resistance, reactance, susp):  # 删除支路的支路追加法
-        node1 = int(node1)
-        node2 = int(node2)
-        self.part_matrix[node1 - 1][node1 - 1] = -susp / 2  
-        self.part_matrix[node2 - 1][node2 - 1] = -susp / 2
-        self.admittance_matrix = self.admittance_matrix + self.part_matrix
-        self.part_matrix = np.zeros((self.bus_num, self.bus_num), dtype=complex)  # 辅助矩阵置零
+    def minus(self, node1, node2, resistance, reactance, total_b):  # 删除支路的支路追加法
         if resistance == 0 and reactance == 0:
             return
         impedance = complex(resistance, reactance)  # 计算阻抗
         admittance = 1 / impedance  # 计算导纳
+        average_b = complex(0, total_b / 2)  # 计算分配在两端母线上的电纳
+        node1 = int(node1)
+        node2 = int(node2)
+        if total_b != 0:    # 对节点导纳矩阵添加并联支路电纳
+            self.part_matrix[node1 - 1][node1 - 1] = -average_b
+            self.part_matrix[node2 - 1][node2 - 1] = -average_b
+            self.admittance_matrix = self.admittance_matrix + self.part_matrix
+            self.part_matrix = np.zeros((self.bus_num, self.bus_num), dtype=complex)  # 辅助矩阵置零
         if node1 != node2:  # 判断不是并联导纳
-            self.part_matrix[node1 - 1][node1 - 1] = -admittance + susp / 2  # 根据增加支路修改辅助矩阵元素
-            self.part_matrix[node2 - 1][node2 - 1] = -admittance + susp / 2
+            self.part_matrix[node1 - 1][node1 - 1] = -admittance  # 根据增加支路修改辅助矩阵元素
+            self.part_matrix[node2 - 1][node2 - 1] = -admittance
             self.part_matrix[node1 - 1][node2 - 1] = admittance
             self.part_matrix[node2 - 1][node1 - 1] = admittance
             self.admittance_matrix = self.admittance_matrix + self.part_matrix  # 支路追加
             self.part_matrix = np.zeros((self.bus_num, self.bus_num), dtype=complex)  # 辅助矩阵置零
+        else:  # 并联导纳
+            self.part_matrix[node1 - 1][node1 - 1] = -admittance
+            self.admittance_matrix = self.admittance_matrix + self.part_matrix
+            self.part_matrix[node1 - 1][node1 - 1] = 0
 
     def get_matrix(self):
         return self.admittance_matrix
 
-    def generate_matrix(self, nodelist1, nodelist2, resistance, reactance, susp,  conductance, susceptance):
+    def generate_matrix(self, nodelist1, nodelist2, resistance, reactance, total_b, conductance, susceptance):
         for i in range(len(nodelist1)):
-            self.add(nodelist1[i], nodelist2[i], resistance[i], reactance[i], susp[i])
+            self.add(nodelist1[i], nodelist2[i], resistance[i], reactance[i], total_b[i])
         for i in range(self.bus_num):
             self.set_self_ad(i+1, conductance[i], susceptance[i])
 
