af7f65fb25c04572358ebd087a16ba60208152c3
biaheart <44721134+biaheart@users.noreply.github.com>
2020-06-12 09:10:23 +0800

    为项目配置了一个虚拟环境

--- flaskweb/lib_packages/__init__.py
+++ flaskweb/lib_packages/__init__.py
@@ -1,1 +1,1 @@
-__all__ = ['data_preprocessing', 'Admittance_Matrix_Class', 'Load_Flow_Calculation']
+__all__ = ['data_preprocessing', 'Admittance_Matrix_Class', 'Load_Flow_Calculation', 'power_distribution']
